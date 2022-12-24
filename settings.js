f8ad0cf7507201446a99ef6eff92f86107532db1
Fajar Alfarizi <76648497+Tersakiti404-cyber@users.noreply.github.com>
2022-12-24 23:42:39 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,25 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 
 // Other
-// Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "𝐹𝑒𝑙𝑖𝑥-𝑀𝐷" //namabot kalian
+global.ownername= "Fajar Alfarizi" //nama kalian
+global.myweb ="https://github.com/Tersakiti404-cyber" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@felixzyra" //bebas asal jan hapus
+global.myweb2 = "https://github.com/Yuni-ecasari" //bebas
+global.email = "zyrafelix@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6281333782061'] //premium user
+global.owner = ["6281333782061","6281333782061"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6281333782061" // nomor wa kalian
+global.ownernomerr = "+6281333782061" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Created By Fajar Alfarizi' //sticker wm ubah
+global.author = '© Jika ingin chat owner:\nWA: +62 813-3378-2061\nFB: Itz Kirito Kun\nIG: @mhmdfjralfarizi_\nTele: @ItzSadnessKun' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
